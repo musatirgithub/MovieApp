086767803fd83aa276ec5632e295a5c9229ff5fa
Mehmet Ugur SATIR <musatir@gmail.com>
2022-07-19 00:46:48 +0300

    .env file updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 import toast from 'react-hot-toast';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB9ddtzKdNaPLfetueBAu8Ar3oyIoryl4A",
-    authDomain: "movieapp-c35db.firebaseapp.com",
-    projectId: "movieapp-c35db",
-    storageBucket: "movieapp-c35db.appspot.com",
-    messagingSenderId: "935367212026",
-    appId: "1:935367212026:web:83e595785254df56140243"
+    apiKey: process.env.REACT_APP_AUTH_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
 };
 
 
