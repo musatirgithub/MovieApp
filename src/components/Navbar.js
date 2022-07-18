99f7f06e3ce8278a3f0ee5a80196ed4759686ba4
Mehmet Ugur SATIR <musatir@gmail.com>
2022-07-19 01:18:23 +0300

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,24 +17,14 @@ const Navbar = () => {
 
   return (
     
-    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+    <nav className="navbar navbar-expand-md navbar-light bg-primary">
     <Toaster position="top-right" toastOptions={{style:{color:'black'}}}/>
       <div className="container-fluid">
         <Link className="navbar-brand ms-4 text-light" to="/">
           Shutter Movie App
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <div className="" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0 gap-4">
           {loggedUser ? <li className="nav-item text-light"><button className="btn btn-primary">Welcome {loggedUser}</button></li>:<li className="nav-item">
               <button className="btn btn-primary border border-light" onClick={()=>navigate('/login')}>Login</button>
